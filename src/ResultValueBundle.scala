35b231726ad131890792881eca776071ac51dbdb
MrabEzreb <de013b3f@opayq.com>
2015-04-17 10:45:47 -0400

    OH GOSH! Github says that my initial commit is more java than scala! MUST BE FIXED! :D

---
+++ src/ResultValueBundle.scala
@@ -0,0 +1,9 @@
+class ResultValueBundle[A](startResult: Boolean, startValue: A) {
+  var result = startResult
+  var value = startValue
+  println("Created a ResultValue Bundle with value type of "+value.getClass().getSimpleName())
+  override def toString(): String = {
+    var string = value+": "+result
+    string
+  }
+}
--- src/TotalNumbers.scala
+++ src/TotalNumbers.scala
@@ -2,6 +2,10 @@ object TotalNumbers extends App {
 
   override def main(args: Array[String]): Unit = {
     impCoding()
+    funcCoding()
+    numbersmap()
+    val rvb = new ResultValueBundle[Int](true, 50)
+    print(rvb.toString())
   }
   def impCoding(): Unit = {
     var list = List(1,2,3,4,5,6)
@@ -9,5 +13,15 @@ object TotalNumbers extends App {
     var total = 0
     for(value <- list) {      total += value    }
     println(total)
+  }
+  def funcCoding(): Unit = {
+    var list = List(1,2,3,4,5,6)
+    println(
+        list.foldLeft(0){ (c, e) => c + e }
+    )
+  }
+  def numbersmap(): Unit = {
+    val numbers = List(1,2,3,4,5,6)
+    println(numbers.map { e => e * 2 })
   }
 }
